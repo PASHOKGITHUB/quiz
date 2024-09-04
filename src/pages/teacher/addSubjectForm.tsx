0d361bd90eeecdd9d7f02fa9f89f7ca9146b2973
PASHOKGITHUB <pashokbabu.38@gmail.com>
2024-09-04 16:03:36 +0530

    subject

--- src/pages/teacher/addSubjectForm.tsx
+++
@@ -1,9 +0,0 @@
-// // pages/teachers/subject/add-subject-form.tsx
-
-// import AddSubjectForm from '@/components/Teachers/Subject/AddSubjectForm';
-
-// const AddSubjectFormPage = () => {
-//     return <AddSubjectForm />;
-// };
-
-// export default AddSubjectFormPage;
